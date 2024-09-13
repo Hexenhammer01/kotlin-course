0282b5974da141e916919ef34f83477f8affde12
Hexenhammer01 <xpp94@mail.ru.example>
2024-09-14 01:19:33 +0300

    lesson 3
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

---
+++ src/main/kotlin/com/rail/kotlincourse/lesson3/lesson 3.kt
@@ -0,0 +1,17 @@
+package com.rail.kotlincourse.lesson3
+
+package com.rail.com.rail.kotlincourse
+
+val name: String = "Название мероприятия" //Неизменяемое
+
+const val date = "01.01.2025"  //Неизменяемое
+
+val place: String="red square" //Неизменяемое
+
+lateinit var budget : String
+
+val lazyValue:String by lazy {
+    ""
+}
+
+val information : String = "Контактная информация"
