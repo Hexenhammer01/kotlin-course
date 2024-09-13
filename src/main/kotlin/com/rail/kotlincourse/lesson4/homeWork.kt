30053c8a190c77c9fb6a13035972fe5ff5e9352e
Hexenhammer01 <xpp94@mail.ru.example>
2024-09-14 02:11:28 +0300

    некоторые не знаю
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

---
+++ src/main/kotlin/com/rail/kotlincourse/lesson4/homeWork.kt
@@ -0,0 +1,46 @@
+package com.rail.kotlincourse.lesson4
+
+//Для каждого значения допиши подходящий тип данных
+
+
+
+val myByte : Byte = 42
+val myLong : Long = 98765432123456789L
+val myFloat : Float = 23.45f
+val myDouble : Double = 0.123456789
+val myString : String = "Kotlin & Java"
+val myBoolean : Boolean = false
+val myChar : Char = 'c'
+val myInt : Int = 500
+val myLong : Long = 4294967296L
+val myFloat : Float = 18.0f
+val myDouble : Double = -0.001
+val myString : String = "OpenAI"
+val myBoolean : Boolean = “true”
+val myChar : Char = '9'
+val myInt : Int = 2048
+val myLong : Long = 10000000000L
+val myFloat : Float = 5.75f
+val myDouble : Double = 1.414
+val myString : String = "Artificial Intelligence"
+val myBoolean : Boolean = false
+val myChar : Char = '@'
+val myInt : Int = 1024
+val myLong : Long = 234567890123L
+val myFloat : Float = 10.01f
+val myDouble : Double = -273.15
+val myString : String = "SpaceX"
+val myBoolean : Boolean = true
+val anything : Any = “🤯”
+val anything : Any = ‘65535’
+val myLong : Long = 72057594037927935L
+val myFloat : Float = 2.71828f
+val myDouble : Double = 101.0101
+val myString : String = "Quantum Computing"
+val myBoolean : Boolean = false
+val myChar : Char = 'x'
+val myInt : Int = 314
+val myLong : Long = 123456789123456789L
+val myFloat : Float = 6.626f
+val myDouble : Double = 0.007
+val myString : String = "Android Studio"
