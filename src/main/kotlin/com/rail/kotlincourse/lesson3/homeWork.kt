33d5366c5a5c55b57c145ff695fdf0153f12943c
Hexenhammer01 <xpp94@mail.ru.example>
2024-09-17 09:16:23 +0300

    переделал домашку №3
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

--- src/main/kotlin/com/rail/kotlincourse/lesson3/homeWork.kt
+++ src/main/kotlin/com/rail/kotlincourse/lesson3/homeWork.kt
@@ -1,16 +1,38 @@
 package com.rail.kotlincourse.lesson3
 
 
-val name: String = "Название мероприятия" //Неизменяемое
-
-const val date = "01.01.2025"  //Неизменяемое
-
-val place: String="red square" //Неизменяемое
-
-lateinit var budget : String
-
-val lazyValue:String by lazy {
-    ""
-}
-
 val information : String = "Контактная информация"
+
+
+val eventName: String = "Название мероприятия"
+
+const val eventDate : String = "01.01.2001"
+
+val eventLocation: String = "Локация"
+
+private val budget: String by lazy { "файл" }
+
+val numberOfParticipants: Int = 0
+
+val eventDuration: Int = 60*60*8
+
+private val contactInfo: String = "Контакты"
+
+var currentState: String = "Статус"
+
+val sponsors: String = "Спонсоры"
+
+val Budget: Double = 1000.0
+
+var currentInternetAccessLevel: String = "уровень интернета"
+
+private val info: String = "Инфа о оборудовании"
+
+val numberOfTeams: Int = 99
+
+var evacuationPlan: String = ""
+
+const val availableEquipment: String = "план куда бежать"
+
+var freeEquipment: String = "Список оборудования"
+
