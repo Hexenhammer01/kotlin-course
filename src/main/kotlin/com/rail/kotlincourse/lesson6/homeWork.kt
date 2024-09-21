f0b4b9f06ee69c375357c18bc4dc91d74918e419
Hexenhammer01 <xpp94@mail.ru.example>
2024-09-22 01:34:26 +0300

    Осталась только задача 6
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

--- src/main/kotlin/com/rail/kotlincourse/lesson6/homeWork.kt
+++ src/main/kotlin/com/rail/kotlincourse/lesson6/homeWork.kt
@@ -1,5 +1,5 @@
 package com.rail.kotlincourse.lesson6
-Задача 1
+//Задача 1
 fun main(){
     println(getSeason(season = 345))
 }
@@ -37,7 +37,7 @@ fun transpotType (transport:Double):String{
         "автотранспорт"
     }
 }
-Задача 4
+//Задача 4
 fun main(){
     println(clientPoints(points=2000))
 }
@@ -48,7 +48,7 @@ fun clientPoints(points:Int):Int{
         else -> 5*(points/100)
     }
 }
-Задача 5
+//Задача 5
 fun main(){
     println(getSystemFile(file="txt"))
 }
@@ -65,3 +65,28 @@ fun getSystemFile(file:String):String{
     }
 }
 //Задача 6
+//Задача 7
+fun main(){
+    println( outsideTemp(temp=0))
+}
+fun outsideTemp(temp:Int):String{
+    return when(temp){
+        in -30 until  0 -> "Куртка и шорты"
+        in 15 downTo 0 -> "ветровка"
+        in 35 downTo 15 -> "футболка и шорты"
+        else -> "Не выходи из дома"
+    }
+}
+//Задача 8
+fun main(){
+    println(movieRate(age=99))
+}
+fun movieRate(age:Int):String{
+    return if (age in 0..6){
+        "детские"
+    }else if (age in 7..17){
+        "подростковые"
+    }else {
+        "18+"
+    }
+}
