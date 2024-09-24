22e30abda51e13f3f1d6b2378bd4957c8517cac7
Hexenhammer01 <xpp94@mail.ru.example>
2024-09-25 02:05:06 +0300

    Урок 7
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

--- src/main/kotlin/com/rail/kotlincourse/lesson6/homeWork.kt
+++ src/main/kotlin/com/rail/kotlincourse/lesson6/homeWork.kt
@@ -1,92 +1,92 @@
 package com.rail.kotlincourse.lesson6
 //Задача 1
-fun main(){
-    println(getSeason(season = 345))
-}
-fun getSeason (season:Int):String {
-    return when (season) {
-        in 3 until 6 -> "spring"
-        in 6 until  9 -> "summer"
-        in 9 until 12 -> "autum"
-        12 -> "winter"
-        in 1 until 3 ->"winter"
-        else->"Incorrect"
-    }
-}
-
-//Задача 2
-fun HumanAge(dogAge: Int): Double {
-    return if (dogAge <= 2) {
-        dogAge * 10.5
-    } else {
-        2 * 10.5 + (dogAge - 2) * 4
-    }
-}
-
-//Задача 3
-fun main(){
-    println(transpotType(transport = 4.9))
-}
-
-fun transpotType (transport:Double):String{
-    return if (transport <1){
-        "пешком"
-    }else if (transport >1 && transport <5){
-        "велосипед"
-    }else {
-        "автотранспорт"
-    }
-}
-//Задача 4
-fun main(){
-    println(clientPoints(points=2000))
-}
-
-fun clientPoints(points:Int):Int{
-    return when (points){
-        in 1..1000 -> 2*(points/100)
-        else -> 5*(points/100)
-    }
-}
-//Задача 5
-fun main(){
-    println(getSystemFile(file="txt"))
-}
-
-fun getSystemFile(file:String):String{
-    return if (file.equals("txt") ){
-        "Текстовый документ"
-    }else if (file.equals("png")){
-        "Изображение"
-    }else if (file.equals("xls")){
-        "Документ"
-    }else {
-        "Неизвестный тип"
-    }
-}
-//Задача 6
-//Задача 7
-fun main(){
-    println( outsideTemp(temp=0))
-}
-fun outsideTemp(temp:Int):String{
-    return when(temp){
-        in -30 until  0 -> "Куртка и шорты"
-        in 15 downTo 0 -> "ветровка"
-        in 35 downTo 15 -> "футболка и шорты"
-        else -> "Не выходи из дома"
-    }
-}
-//Задача 8
-fun main(){
-    println(movieRate(age=99))
-}
-fun movieRate(age:Int):String{
-    return if (age in 0..6){
-        "детские"
-    }else if (age in 7..17){
-        "подростковые"
-    }else {
-        "18+"
-    }
-}
+//fun main(){
+//    println(getSeason(season = 345))
+//}
+//fun getSeason (season:Int):String {
+//    return when (season) {
+//        in 3 until 6 -> "spring"
+//        in 6 until  9 -> "summer"
+//        in 9 until 12 -> "autum"
+//        12 -> "winter"
+//        in 1 until 3 ->"winter"
+//        else->"Incorrect"
+//    }
+//}
+//
+////Задача 2
+//fun HumanAge(dogAge: Int): Double {
+//    return if (dogAge <= 2) {
+//        dogAge * 10.5
+//    } else {
+//        2 * 10.5 + (dogAge - 2) * 4
+//    }
+//}
+//
+////Задача 3
+//fun main(){
+//    println(transpotType(transport = 4.9))
+//}
+//
+//fun transpotType (transport:Double):String{
+//    return if (transport <1){
+//        "пешком"
+//    }else if (transport >1 && transport <5){
+//        "велосипед"
+//    }else {
+//        "автотранспорт"
+//    }
+//}
+////Задача 4
+//fun main(){
+//    println(clientPoints(points=2000))
+//}
+//
+//fun clientPoints(points:Int):Int{
+//    return when (points){
+//        in 1..1000 -> 2*(points/100)
+//        else -> 5*(points/100)
+//    }
+//}
+////Задача 5
+//fun main(){
+//    println(getSystemFile(file="txt"))
+//}
+//
+//fun getSystemFile(file:String):String{
+//    return if (file.equals("txt") ){
+//        "Текстовый документ"
+//    }else if (file.equals("png")){
+//        "Изображение"
+//    }else if (file.equals("xls")){
+//        "Документ"
+//    }else {
+//        "Неизвестный тип"
+//    }
+//}
+////Задача 6
+////Задача 7
+//fun main(){
+//    println( outsideTemp(temp=0))
+//}
+//fun outsideTemp(temp:Int):String{
+//    return when(temp){
+//        in -30 until  0 -> "Куртка и шорты"
+//        in 15 downTo 0 -> "ветровка"
+//        in 35 downTo 15 -> "футболка и шорты"
+//        else -> "Не выходи из дома"
+//    }
+//}
+////Задача 8
+//fun main(){
+//    println(movieRate(age=99))
+//}
+//fun movieRate(age:Int):String{
+//    return if (age in 0..6){
+//        "детские"
+//    }else if (age in 7..17){
+//        "подростковые"
+//    }else {
+//        "18+"
+//    }
+//}
---
+++ src/main/kotlin/com/rail/kotlincourse/lesson7/lesson7.kt
@@ -0,0 +1,33 @@
+package com.rail.kotlincourse.lesson7
+
+fun main(){
+    for (i in 1..3){
+        println(i)
+    }
+}
+
+fun main (){
+    val range = 1..6
+    for (i in range){
+        println(i)
+    }
+}
+
+fun main(){
+    while(true){
+        println("+")
+    }
+}
+
+fun main(){
+    var counter = 10
+    while (counter-- > 0){
+        println()
+    }
+}
+
+fun main(){
+    do {
+
+    }while (condition)
+}
