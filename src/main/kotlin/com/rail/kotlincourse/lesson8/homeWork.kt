c0e52be90e35f4ac68594c08ac076ac89e5d83fe
Hexenhammer01 <xpp94@mail.ru.example>
2024-09-29 14:11:57 +0300

    урок 8 и чуть домашки
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

--- src/main/kotlin/com/rail/kotlincourse/lesson8/homeWork.kt
+++ src/main/kotlin/com/rail/kotlincourse/lesson8/homeWork.kt
@@ -1,5 +1,5 @@
 package com.rail.kotlincourse.lesson8
-fun main(){
+fun main1(){
     val origFraze = "Это невозможно выполнить за один день"
     val origFraze1 = "Я не уверен в успехе этого проекта"
     val origFraze2 = "Произошла катастрофа на сервере"
@@ -7,23 +7,51 @@ fun main(){
     val origFraze4 = "Удача"
     println(fraze(origFraze))
     println(fraze1(origFraze1))
+    println(fraze2(origFraze2))
+    println(fraze3(origFraze3))
+    println(fraze4(origFraze4))
+
+
 }
 fun fraze(origFraze:String):String{
     //Если фраза содержит слово "невозможно":
     //Преобразование: Замените "невозможно" на "совершенно точно возможно, просто требует времени".
     return when {
         origFraze.contains("невозможно") -> origFraze.replace("невозможно", "совершенно точно возможно, просто требует времени", true )
-        else -> ""
+        else -> " "
     }
 }
 
-fun fraze1(origFraze1: String):String {
+fun fraze1(origFraze1:String):String {
     return when {
         origFraze1.startsWith("Я не уверен", true) -> "$origFraze1, но моя интуиция говорит об обратном"
-        else -> ""
+        else -> " "
     }
 }
-
-
+fun fraze2(origFraze2:String): String{
+    return when {
+        origFraze2.contains("катастрофа", true) -> origFraze2.replace("катастрофа", "интересное событие")
+        else -> " "
+    }
+}
+fun fraze3(origFraze3:String):String{
+    return when {
+        origFraze3.endsWith("без проблем", true ) -> origFraze3.replace("без проблем","с парой интересных вызовов на пути")
+        else-> " "
+    }
+}
+fun fraze4(origFraze4:String):String{
+    return when{
+        origFraze4.contains("Иногда",true) -> "Иногда ${origFraze4.lowercase()}, но не всегда"
+        else -> " "
+    }
+}
+fun main(){
+    val i = "Пользователь вошел в систему -> 2021-12-01 09:48:23"
+    val i1 = "Пользователь вошел в систему -> 2021-12-01 09:48:23".indexOf(">")
+//    println(i1)
+    println(i.substring(i1+2,i1+12))
+    println(i.substring(i1+13,i1+21))
+}
 
 
