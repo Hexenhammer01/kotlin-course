4be607f46e2a1eedf69684e822793b33cae49346
Hexenhammer01 <xpp94@mail.ru.example>
2024-09-29 17:40:06 +0300

    домашка
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

--- src/main/kotlin/com/rail/kotlincourse/lesson8/homeWork.kt
+++ src/main/kotlin/com/rail/kotlincourse/lesson8/homeWork.kt
@@ -1,57 +1,82 @@
 package com.rail.kotlincourse.lesson8
-fun main1(){
+
+import javax.swing.plaf.OptionPaneUI
+
+fun main() {
     val origFraze = "Это невозможно выполнить за один день"
     val origFraze1 = "Я не уверен в успехе этого проекта"
     val origFraze2 = "Произошла катастрофа на сервере"
     val origFraze3 = "Этот код работает без проблем"
     val origFraze4 = "Удача"
-    println(fraze(origFraze))
-    println(fraze1(origFraze1))
-    println(fraze2(origFraze2))
-    println(fraze3(origFraze3))
-    println(fraze4(origFraze4))
+//    println(origFraze)
+//    println(origFraze1)
+//    println(origFraze2)
+//    println(origFraze3)
+//    println(origFraze4)
 
 
-}
-fun fraze(origFraze:String):String{
-    //Если фраза содержит слово "невозможно":
-    //Преобразование: Замените "невозможно" на "совершенно точно возможно, просто требует времени".
-    return when {
-        origFraze.contains("невозможно") -> origFraze.replace("невозможно", "совершенно точно возможно, просто требует времени", true )
-        else -> " "
+    fun fraze(origFraze: String): String {
+        //Если фраза содержит слово "невозможно":
+        //Преобразование: Замените "невозможно" на "совершенно точно возможно, просто требует времени".
+        return when {
+            origFraze.contains("невозможно") -> origFraze.replace("невозможно", "совершенно точно возможно, просто требует времени", true)
+            else -> " "
+        }
     }
-}
 
-fun fraze1(origFraze1:String):String {
-    return when {
-        origFraze1.startsWith("Я не уверен", true) -> "$origFraze1, но моя интуиция говорит об обратном"
-        else -> " "
+    fun fraze1(origFraze1: String): String {
+        return when {
+            origFraze1.startsWith("Я не уверен", true) -> "$origFraze1, но моя интуиция говорит об обратном"
+            else -> " "
+        }
     }
-}
-fun fraze2(origFraze2:String): String{
-    return when {
-        origFraze2.contains("катастрофа", true) -> origFraze2.replace("катастрофа", "интересное событие")
-        else -> " "
+
+    fun fraze2(origFraze2: String): String {
+        return when {
+            origFraze2.contains("катастрофа", true) -> origFraze2.replace("катастрофа", "интересное событие")
+            else -> " "
+        }
     }
-}
-fun fraze3(origFraze3:String):String{
-    return when {
-        origFraze3.endsWith("без проблем", true ) -> origFraze3.replace("без проблем","с парой интересных вызовов на пути")
-        else-> " "
+
+    fun fraze3(origFraze3: String): String {
+        return when {
+            origFraze3.endsWith("без проблем", true) -> origFraze3.replace(
+                "без проблем",
+                "с парой интересных вызовов на пути"
+            )
+
+            else -> " "
+        }
     }
-}
-fun fraze4(origFraze4:String):String{
-    return when{
-        origFraze4.contains("Иногда",true) -> "Иногда ${origFraze4.lowercase()}, но не всегда"
-        else -> " "
+
+    fun fraze4(origFraze4: String): String {
+        return when {
+            origFraze4.contains("Иногда", true) -> "Иногда ${origFraze4.lowercase()}, но не всегда"
+            else -> " "
+        }
     }
-}
-fun main(){
+
+
     val i = "Пользователь вошел в систему -> 2021-12-01 09:48:23"
     val i1 = "Пользователь вошел в систему -> 2021-12-01 09:48:23".indexOf(">")
-//    println(i1)
-    println(i.substring(i1+2,i1+12))
-    println(i.substring(i1+13,i1+21))
+    println(i1)
+    println(i.substring(i1 + 2, i1 + 12))
+    println(i.substring(i1 + 13, i1 + 21))
+
+
+    val card = "4539 1488 0343 6467"
+    val splitCard = card.split(" ")
+    println("**** **** **** ${splitCard[3]}")
+
+
+    val email = "username@example.com"
+    val newEmail = email.replace("@", " [at] ").replace(".", " [dot] ")
+    println(email)
+
+    val way = "C:/Пользователи/Документы/report.txt"
+    val splitWay = way.split("/")
+    println(splitWay.last())
+
+
+
 }
-
-
--- src/main/kotlin/com/rail/kotlincourse/lesson8/lesson8.kt
+++ src/main/kotlin/com/rail/kotlincourse/lesson8/lesson8.kt
@@ -3,7 +3,7 @@ package com.rail.kotlincourse.lesson8
 import com.rail.kotlincourse.lesson4.printMessage
 
 
-fun main() {
+//fun main() {
 //    val simpleString = "Это простая строка"
 //
 //    val firsName = "Иван"
@@ -31,80 +31,80 @@ fun main() {
 //
 //fun getDetails(): String{
 //    return "Очень интересные детали"
-
-    val originalString = "Kotlin is fun"
-    val subString = originalString.substring(startIndex = 7)
-    val subString2 = originalString.substring(3, 6)
-    val replaceString = originalString.replace(oldValue = "fun", newValue = "awesome")
-    val words = originalString.split(" ")
-    val lenght = "Hello".length
-    val upper = "hello".uppercase()
-    val lower = "HELLO".lowercase()
-    val trimmed = "    hello.   ".trim()
-    val start = "Kotlin".startsWith("Kot")
-    val ends = "Kotlin".endsWith("lin")
-    val cont = "Hello".contains("ell")
-    val e1: String? = null
-    val empty = e1.isNullOrEmpty()
-    val blank = "  ".isNullOrBlank()
-    val repeat = "ab".repeat(3)
-    val letter = originalString[5]
-    val indexofChar = "Kotlin".indexOf('h')
-    val indexofWord = "Kotlin is the best".indexOf("best")
-
-    val backRevers = "niltoK".reversed()
-    val multi = """
-               Первая строка
-             Вторая строка
-           Третья строка
-""".trimIndent()
-    val string = "Swit summer child"
-    val subStringq = string.substring(6)
-    val subls = string.substring(6, 12)
-
-    val name = "Alex"
-    val city = "MSK"
-    val age = 33
-    val fCount = 123
-    val rating = 4.232
-    val balance = 23423.13513
-    val text = """
-        Имя: %s
-        Город: %s
-        Возраст: %d
-        Количество друзей: %,d
-        Рейтинг пользователя: %.1f
-        Баланс счета: $%,.2f
-    """.trimIndent()
-//    println(text.format(name, city, age, fCount, rating, balance))
-
-    println(convert("Для завершения проекта важно"))
-    val s = "Kotlin: The Fun Way To Learn Programming"
-    println(s.substring(8,19))
-
-    val e = "contact@examle.ru".indexOf("@")
-    val e3 = "contact@examle.ru"
-    println(e3.substring(e + 1))
-
-    val tel ="123-456-7890"
-    val tel1 = tel.substring(8,)
-    val split = tel.split("-")
-    println("XXX-XXX-${tel1}")
-    println("XXX-XXX-${tel.substring(8)}")
-
-    println("XXX-XXX-${split[2]}")
-    val origFraze = "Это невозможно выполнить за один день"
-    println(fraze(origFraze))
-
-}
-
-fun convert(string: String):String{
-    return when{
-        string.startsWith("ошибка", true) -> string.replace(oldValue = "ошибка", newValue = "небольшое недорозумение",true)
-        string.endsWith("важно",true) -> "$string...но не критично"
-        else -> ""
-    }
-}
+//
+//    val originalString = "Kotlin is fun"
+//    val subString = originalString.substring(startIndex = 7)
+//    val subString2 = originalString.substring(3, 6)
+//    val replaceString = originalString.replace(oldValue = "fun", newValue = "awesome")
+//    val words = originalString.split(" ")
+//    val lenght = "Hello".length
+//    val upper = "hello".uppercase()
+//    val lower = "HELLO".lowercase()
+//    val trimmed = "    hello.   ".trim()
+//    val start = "Kotlin".startsWith("Kot")
+//    val ends = "Kotlin".endsWith("lin")
+//    val cont = "Hello".contains("ell")
+//    val e1: String? = null
+//    val empty = e1.isNullOrEmpty()
+//    val blank = "  ".isNullOrBlank()
+//    val repeat = "ab".repeat(3)
+//    val letter = originalString[5]
+//    val indexofChar = "Kotlin".indexOf('h')
+//    val indexofWord = "Kotlin is the best".indexOf("best")
+//
+//    val backRevers = "niltoK".reversed()
+//    val multi = """
+//               Первая строка
+//             Вторая строка
+//           Третья строка
+//""".trimIndent()
+//    val string = "Swit summer child"
+//    val subStringq = string.substring(6)
+//    val subls = string.substring(6, 12)
+//
+//    val name = "Alex"
+//    val city = "MSK"
+//    val age = 33
+//    val fCount = 123
+//    val rating = 4.232
+//    val balance = 23423.13513
+//    val text = """
+//        Имя: %s
+//        Город: %s
+//        Возраст: %d
+//        Количество друзей: %,d
+//        Рейтинг пользователя: %.1f
+//        Баланс счета: $%,.2f
+//    """.trimIndent()
+////    println(text.format(name, city, age, fCount, rating, balance))
+//
+//    println(convert("Для завершения проекта важно"))
+//    val s = "Kotlin: The Fun Way To Learn Programming"
+//    println(s.substring(8,19))
+//
+//    val e = "contact@examle.ru".indexOf("@")
+//    val e3 = "contact@examle.ru"
+//    println(e3.substring(e + 1))
+//
+//    val tel ="123-456-7890"
+//    val tel1 = tel.substring(8,)
+//    val split = tel.split("-")
+//    println("XXX-XXX-${tel1}")
+//    println("XXX-XXX-${tel.substring(8)}")
+//
+//    println("XXX-XXX-${split[2]}")
+//    val origFraze = "Это невозможно выполнить за один день"
+//    println(fraze(origFraze))
+//
+//}
+//
+//fun convert(string: String):String{
+//    return when{
+//        string.startsWith("ошибка", true) -> string.replace(oldValue = "ошибка", newValue = "небольшое недорозумение",true)
+//        string.endsWith("важно",true) -> "$string...но не критично"
+//        else -> ""
+//    }
+//}
 //fun fraze(origFraze:String):String{
 //    //Если фраза содержит слово "невозможно":
 //    //Преобразование: Замените "невозможно" на "совершенно точно возможно, просто требует времени".
