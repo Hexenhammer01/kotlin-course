4982812e34246a5d23adf95cadc0eaa03985ed77
Hexenhammer01 <xpp94@mail.ru.example>
2024-10-20 17:09:48 +0300

    урок 14
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

--- src/main/kotlin/com/rail/kotlincourse/lesson14/lesson14.kt
+++ src/main/kotlin/com/rail/kotlincourse/lesson14/lesson14.kt
@@ -11,4 +11,61 @@ fun main() {
         println("Key : ${fruit}, Value: ${price}")
     }
 
+    val entries = fruitsPrice.entries
+
+    val keys = fruitsPrice.keys
+
+    val values = fruitsPrice.values
+
+    val priceOfPearOrElse = fruitsPrice.getOrElse("pear") { throw IllegalArgumentException() }
+
+    val priceOfApple = fruitsPrice.getValue("apple")
+
+    val priceOfPearOrDefault = fruitsPrice.getOrDefault("pear", 0.0)
+
+    val fruitsWithPineapple = fruitsPrice + ("pineapple" to 3.5)
+
+    val mutableFruits = fruitsPrice.toMutableMap()
+
+    mutableFruits["kiwi"] = 2.75 // Добавление элемента
+
+    mutableFruits.putAll(mapOf("lime" to 1.1, "avocado" to 1.9))
+
+    mutableFruits.remove("apple") // Удаление элемента
+
+    mutableFruits.clear()
+
+    val containsApple = fruitsPrice.containsKey("apple")
+
+    val containsValue1_5 = fruitsPrice.containsValue(1.5)
+
+    val isEmpty = fruitsPrice.isEmpty()
+
+    val isNotEmpty = fruitsPrice.isNotEmpty()
+
+    val areAllFruitsExpensive = fruitsPrice.all { it.value > 1.0 }
+
+    val isAnyFruitCheap = fruitsPrice.any { it.value < 1.0 }
+
+    val filteredByPrice = fruitsPrice.filter { it.value > 1.0 }
+
+    val filteredByKeys = fruitsPrice.filterKeys { it.startsWith("a") }
+
+    val filteredByValues = fruitsPrice.filterValues { it < 2.0 }
+
+    val filteredNotApple = fruitsPrice.filterNot { it.key == "apple" }val countExpensiveFruits = fruitsPrice.count { it.value > 1.5 }
+
+    val increasedPrices = fruitsPrice.mapValues { it.value * 1.1 }
+
+    val fruitNamesUppercase = fruitsPrice.mapKeys { it.key.uppercase() }
+
+    val fruitsList = fruitsPrice.map { "${it.key} costs ${it.value}" }
+
+
+
+
+
+
+
+
 }
