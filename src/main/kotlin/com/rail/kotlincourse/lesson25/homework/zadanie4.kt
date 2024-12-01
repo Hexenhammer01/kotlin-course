44b00cd4df597b5d65e0db858ec419fe2633a88b
Hexenhammer01 <xpp94@mail.ru.example>
2024-12-01 18:54:04 +0300

    Домашка  25
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

--- src/main/kotlin/com/rail/kotlincourse/lesson25/homework/zadanie4.kt
+++ src/main/kotlin/com/rail/kotlincourse/lesson25/homework/zadanie4.kt
@@ -1,5 +1,7 @@
 package com.rail.kotlincourse.lesson25.homework
-
+////Задание 4. Создай функцию, которая принимает большое число и возвращает сумму цифр этого числа.
+////По аналогии с предыдущим заданием выполни трансформации и проверки.
+////Подсказка: для парсинга строки “3” в число 3 можно использовать “3”.digitToInt()
 fun zadanie4(r: Any): Int {
     require(r.toString().isNotEmpty()) { "Пустой" }
     return r.toString().sumOf { it.digitToInt() }
