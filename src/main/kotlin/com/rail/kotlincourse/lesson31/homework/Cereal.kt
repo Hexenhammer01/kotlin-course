b47cabe8aacd5d1555f239b0778011ae6c0215f3
Hexenhammer01 <xpp94@mail.ru.example>
2024-12-23 09:27:48 +0300

    домашка 31
    Signed-off-by: Hexenhammer01 <xpp94@mail.ru.example>

---
+++ src/main/kotlin/com/rail/kotlincourse/lesson31/homework/Cereal.kt
@@ -0,0 +1,9 @@
+package com.rail.kotlincourse.lesson31.homework
+
+enum class Cereal(val local: String) {
+    BUCKWHEAT("Гречка"),
+    RICE("Рис"),
+    MILLET("Пшено"),
+    PEAS("Горох"),
+    BULGUR("Булгур")
+}
